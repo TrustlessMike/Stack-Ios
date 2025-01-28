f679ba0af3218ea1cfe30bffb7e9c2eff5374176
TrustlessMike <trustless.mike@proton.me>
2025-01-28 18:35:39 -0500

    Initial Commit

---
+++ Stack/StackApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackApp.swift
+//  Stack
+//
+//  Created by Malik Amine on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct StackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
